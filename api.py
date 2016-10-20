0f5698eb65e933f3e46fd6fe0600e0741dc3b03f
navjot <mail.navjot77@gmail.com>
2016-10-20 14:37:45 -0700

    Hangman Code + Skelptn code for guess_a_number

---
+++ api.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-`
+"""api.py - Create and configure the Game API exposing the resources.
+This can also contain game logic. For more complex games it would be wise to
+move game logic to another file. Ideally the API will be simple, concerned
+primarily with communication to/from the API's users."""
+
+
+import logging
+import endpoints
+from protorpc import remote, messages
+from google.appengine.api import memcache
+from google.appengine.api import taskqueue
+
+from models import User, Game, Score
+from models import StringMessage, NewGameForm, GameForm, MakeMoveForm,\
+    ScoreForms
+from utils import get_by_urlsafe
+
+NEW_GAME_REQUEST = endpoints.ResourceContainer(NewGameForm)
+GET_GAME_REQUEST = endpoints.ResourceContainer(
+        urlsafe_game_key=messages.StringField(1),)
+MAKE_MOVE_REQUEST = endpoints.ResourceContainer(
+    MakeMoveForm,
+    urlsafe_game_key=messages.StringField(1),)
+USER_REQUEST = endpoints.ResourceContainer(user_name=messages.StringField(1),
+                                           email=messages.StringField(2))
+
+MEMCACHE_MOVES_REMAINING = 'MOVES_REMAINING'
+
+@endpoints.api(name='guess_a_number', version='v1')
+class GuessANumberApi(remote.Service):
+    """Game API"""
+    @endpoints.method(request_message=USER_REQUEST,
+                      response_message=StringMessage,
+                      path='user',
+                      name='create_user',
+                      http_method='POST')
+    def create_user(self, request):
+        """Create a User. Requires a unique username"""
+        if User.query(User.name == request.user_name).get():
+            raise endpoints.ConflictException(
+                    'A User with that name already exists!')
+        user = User(name=request.user_name, email=request.email)
+        user.put()
+        return StringMessage(message='User {} created!'.format(
+                request.user_name))
+
+    @endpoints.method(request_message=NEW_GAME_REQUEST,
+                      response_message=GameForm,
+                      path='game',
+                      name='new_game',
+                      http_method='POST')
+    def new_game(self, request):
+        """Creates new game"""
+        user = User.query(User.name == request.user_name).get()
+        if not user:
+            raise endpoints.NotFoundException('A User with that name does not exist!')
+        try:
+            game = Game.new_game(user.key, 2)
+        except ValueError:
+            raise endpoints.BadRequestException('Maximum must be greater '
+                                                'than minimum!')
+
+        # Use a task queue to update the average attempts remaining.
+        # This operation is not needed to complete the creation of a new game
+        # so it is performed out of sequence.
+        taskqueue.add(url='/tasks/cache_average_attempts')
+        return game.to_form('Good luck playing Guess a Number!')
+
+    @endpoints.method(request_message=GET_GAME_REQUEST,
+                      response_message=GameForm,
+                      path='game/{urlsafe_game_key}',
+                      name='get_game',
+                      http_method='GET')
+    def get_game(self, request):
+        """Return the current game state."""
+        game = get_by_urlsafe(request.urlsafe_game_key, Game)
+        if game:
+            return game.to_form('Time to make a move!')
+        else:
+            raise endpoints.NotFoundException('Game not found!')
+
+    @endpoints.method(request_message=MAKE_MOVE_REQUEST,
+                      response_message=GameForm,
+                      path='game/{urlsafe_game_key}',
+                      name='make_move',
+                      http_method='PUT')
+    def make_move(self, request):
+        """Makes a move. Returns a game state with message"""
+        game = get_by_urlsafe(request.urlsafe_game_key, Game)
+        if game.game_over:
+            return game.to_form('Game already over!')
+
+
+        chosen_word = game.target
+        #player_guess = None  # will hold the players guess
+        guessed_letters = game.letters_guessed  # a list of letters guessed so far
+        word_guessed = []
+        for i in chosen_word:
+            word_guessed.append("-")  # create an unguessed, blank version of the word
+        #joined_word = None  # joins the words in the list word_guessed
+
+        attempts = game.attempts_remaining
+
+        player_guess=request.guess
+        player_guess=player_guess.lower()
+        if not player_guess.isalpha():  # check the input is a letter. Also checks an input has been made.
+            return game.to_form("That is not a letter. Please try again.")
+        elif len(player_guess) > 1:  # check the input is only one letter
+            return game.to_form("That is more than one letter. Please try again.")
+        elif player_guess in guessed_letters:  # check it letter hasn't been guessed already
+            return game.to_form("You have already guessed that letter. Please try again.")
+        else:
+            pass
+
+        guessed_letters.append(player_guess)
+        game.letters_guessed=guessed_letters
+
+        for letter in range(len(chosen_word)):
+            for guess in guessed_letters:
+                if guess == chosen_word[letter]:
+                    word_guessed[letter] = guess  # replace all letters in the chosen word that match the players guess
+        joined_word = "".join(word_guessed)
+
+        if "-" not in word_guessed:  # no blanks remaining
+            game.end_game(True)
+            return game.to_form(
+                "\nCongratulations! You Won. The word is:".format(word_guessed))
+
+        if game.attempts_remaining == 1:
+            game.end_game(False)
+            return game.to_form('All attempts made. YouLoose')
+
+        if player_guess not in chosen_word:
+            game.attempts_remaining -= 1
+            game.put()
+
+
+
+        return game.to_form("Nice Move <{}>. Guess Other Letter (A-Z)".format(joined_word))
+
+    @endpoints.method(response_message=ScoreForms,
+                      path='scores',
+                      name='get_scores',
+                      http_method='GET')
+    def get_scores(self, request):
+        """Return all scores"""
+        return ScoreForms(items=[score.to_form() for score in Score.query()])
+
+    @endpoints.method(request_message=USER_REQUEST,
+                      response_message=ScoreForms,
+                      path='scores/user/{user_name}',
+                      name='get_user_scores',
+                      http_method='GET')
+    def get_user_scores(self, request):
+        """Returns all of an individual User's scores"""
+        user = User.query(User.name == request.user_name).get()
+        if not user:
+            raise endpoints.NotFoundException(
+                    'A User with that name does not exist!')
+        scores = Score.query(Score.user == user.key)
+        return ScoreForms(items=[score.to_form() for score in scores])
+
+    @endpoints.method(response_message=StringMessage,
+                      path='games/average_attempts',
+                      name='get_average_attempts_remaining',
+                      http_method='GET')
+    def get_average_attempts(self, request):
+        """Get the cached average moves remaining"""
+        return StringMessage(message=memcache.get(MEMCACHE_MOVES_REMAINING) or '')
+
+    @staticmethod
+    def _cache_average_attempts():
+        """Populates memcache with the average moves remaining of Games"""
+        games = Game.query(Game.game_over == False).fetch()
+        if games:
+            count = len(games)
+            total_attempts_remaining = sum([game.attempts_remaining
+                                        for game in games])
+            average = float(total_attempts_remaining)/count
+            memcache.set(MEMCACHE_MOVES_REMAINING,
+                         'The average moves remaining is {:.2f}'.format(average))
+
+
+api = endpoints.api_server([GuessANumberApi])
